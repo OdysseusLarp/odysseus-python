a59e1555b4d1210eebe06238196ecdd6ba54e43b
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-06-19 09:37:50 +0300

    use the test backend server in the cloud

--- reactorconsole/reactorconsole.sh
+++ reactorconsole/reactorconsole.sh
@@ -7,7 +7,8 @@ pkill -f -INT reactorlocal.py
 
 source /home/hacklab/.virtualenvs/reactorconsole/bin/activate
 cd ~/devel/odysseus-python/reactorconsole
-./backendcoms.py --id jump_reactor --url http://192.168.1.2 &
+#./backendcoms.py --id jump_reactor --url http://192.168.1.2 &
+./backendcoms.py --id jump_reactor --url https://apps.odysseuslarp.dev --user odysseus --passwd 2024 --verbose
 ./reactorlocal.py
 pkill -f -INT backendcoms.py
 pkill -f -INT backendcoms.py
